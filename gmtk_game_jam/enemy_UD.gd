cd925d2280c2850c5b4e137b3a0175339017630e
shreyaskm51 <shreyaskmarea51@gmail.com>
2025-08-01 22:52:22 +0530

    enemy,killzone,trapdoor

---
+++ gmtk_game_jam/enemy_UD.gd
@@ -0,0 +1,17 @@
+extends Node2D
+const speed =60
+var direction = 1
+@onready var ray_castup: RayCast2D = $RayCastup
+@onready var ray_castdown: RayCast2D = $RayCastdown
+
+func _process(delta: float):
+	if ray_castup.is_colliding():
+		direction = -1
+		Sprite2D.flip_h = true  #cahnge to animated sprite later when we put the design 
+	if ray_castdown.is_colliding():
+		direction = 1
+		Sprite2D.flip_h = false  #cahnge to animated sprite later when we put the design
+	
+	position.y = direction * speed * delta
+func _draw():
+	draw_circle(Vector2.ZERO,50, Color.LEMON_CHIFFON , true,0.0,true)
---
+++ gmtk_game_jam/enemy_lr.gd
@@ -0,0 +1,14 @@
+extends Node2D
+const speed=60
+var direction = 1
+@onready var left_coll = $RayCastleft
+@onready var right_coll = $RayCastright
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float):
+	if left_coll.is_colliding():
+		direction = 1
+		Sprite2D.flip_h = false #cahnge to animated sprite later when we put the design
+	if right_coll.is_colliding():
+		direction = -1
+		Sprite2D.flip_h = true  #cahnge to animated sprite later when we put the design
+	position.x += direction * speed * delta
